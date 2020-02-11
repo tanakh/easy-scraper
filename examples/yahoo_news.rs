bdc797e7d6056d7b76e51501fba7c4a6c4b1f985
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2020-02-11 13:06:26 +0900

    Add examples

---
+++ examples/yahoo_news.rs
@@ -0,0 +1,17 @@
+use easy_scraper::Pattern;
+
+fn main() {
+    let pat = Pattern::new(
+        r#"
+<li class="topicsListItem">
+    <a href="{{url}}">{{title}}</a>
+</li>
+"#,
+    );
+    let doc = reqwest::blocking::get("https://news.yahoo.co.jp/")
+        .unwrap()
+        .text()
+        .unwrap();
+    let ms = pat.matches(&doc);
+    println!("{:#?}", ms);
+}
---
+++ examples/youtube_trending.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let pat = easy_scraper::Pattern::new(
+        r##"
+<li>
+    <div class="yt-lockup-content">
+        <h3 class="yt-lockup-title">
+            <a href="{{url}}">{{title}}</a>
+        </h3>
+        <div class="yt-lockup-byline">
+            <a href="{{channel-url}}">{{channel}}</a>
+        </div>
+        <div class="yt-lockup-meta">
+            <ul class="yt-lockup-meta-info">
+                <li>{{date}}</li>
+                <li>{{view}}</li>
+            </ul>
+        </div>
+    </div>
+</li>
+<li>
+    最近急上昇
+</li>
+"##,
+    );
+
+    let doc = reqwest::blocking::get("https://www.youtube.com/feed/trending")
+        .unwrap()
+        .text()
+        .unwrap();
+    let ms = pat.matches(&doc);
+    println!("{:#?}", ms);
+    println!("{} results", ms.len());
+}
